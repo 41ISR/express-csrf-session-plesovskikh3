275496c92eb1687e35b8321041eb9283f282abce
Pasha <39154112+ktkv419@users.noreply.github.com>
2025-11-25 05:32:12 +0000

    123

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,47 @@
 const db = require("./db")
 const express = require("express")
+const cors = require("cors")
+const bcrypt = require("bcrypt")
+const cookieParser = require("cookie-parser")
+const session = require("express-session")
 
 const app = express()
 
+app.use(cookieParser())
 app.use(express.json())
+app.use(cors({
+    origin: "*",
+    credentials: false,
+    methods: ["GET", "POST", "DELETE", "PUT"],
+    allowedHeaders: ["Content-Type"]
+}))
+app.use(session({
+    secret: "asdasdasdasdasdasd",
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        httpOnly: true,
+        maxAge: 24 * 60 * 60 * 1000,
+        sameSite: "lax",
+        
+    }
+}))
+
+app.post("/signup", (req, res) => {
+    try {
+        const hashed = bcrypt.hashSync(req.body.password, 10)
+        const newUser = db
+            .prepare(`INSERT INTO users (email, password) VALUES (?, ?)`)
+            .run(req.body.email, hashed);
+        const createdUser = db
+            .prepare(`SELECT * FROM users WHERE id = ?`)
+            .get(newUser.lastInsertRowid);
+        res.status(201).json(createdUser)
+    } catch (error) {
+        console.error(error)
+        res.json(error)
+    }
+})
 
 app.listen("3000", () => {
     console.log("Порт3000")
